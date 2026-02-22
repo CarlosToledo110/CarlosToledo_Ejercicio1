98f1a5cd48e7138748de61965a244fa4ef45000e
CarlosToledo110 <carlostoledo1250@gmail.com>
2026-02-22 12:39:43 -0600

    Menú principal del programa de atención médica

--- CarlosToledo_Ejercicio1/Program.cs
+++ CarlosToledo_Ejercicio1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bienvenido, ¿Que tipo de atención necesita?");
+Console.WriteLine("1. Emergencia ");
+Console.WriteLine("2. Consulta");
+Console.WriteLine("3. Pediatría");
+Console.WriteLine("4. Traumatología");
+int opciónmenú = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Edad: ");
+int edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Temperatura (grados centígrados): ");
+double temp = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Nivel de dolor (0-10): ");
+int niveldolor = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Presión Sistólica: ");
+int pressistolica = int.Parse(Console.ReadLine());
